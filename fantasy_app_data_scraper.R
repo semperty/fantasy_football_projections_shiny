5915d407993f7b3d63e336ffb15319a7893a2569
Paul Johnson <paul@paulrjohnson.net>
2023-07-23 22:33:27 +0100

    Clean up repository structure

--- fantasy_app_data_scraper.R
+++
@@ -1,8 +0,0 @@
-# scrape websites for data
-scrape_data(src = c("CBS", "ESPN", "Fantasy Pros", "Fantasy Sharks", 
-                            "FFToday", "NumberFire", "FantasyFootballNerd", 
-                            "NFL", "RTSports", "Walterfootball"),
-                    pos = c("QB", "RB", "WR", "TE"),
-                    season = 2023, 
-                    week = NULL
-)
